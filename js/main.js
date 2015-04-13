fce1446e8e5c3f1cbee815ae8ce4a11c8bc0649d
Lionel Paulus <hello@lionelpaul.us>
2015-04-13 14:09:51 +0200

    New functions

--- js/main.js
+++ js/main.js
@@ -3,9 +3,10 @@
 function user_data(){ // Get and update score, station, tickets, line with cookies
     var score = null;
     var line = null;
+    var station = null;
     
 }
 
-function get_question(){ // 
-    
+function get_question(){ // Find a question in the JSON file
+    var step = null; // 
 }
