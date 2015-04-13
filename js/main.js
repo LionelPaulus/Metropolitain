3838e3d6dc9b8c3ce61f12e8b55dc5d58ff29b04
Lionel Paulus <hello@lionelpaul.us>
2015-04-13 14:03:42 +0200

    Add main.js

---
+++ js/main.js
@@ -0,0 +1,11 @@
+// Functions
+
+function user_data(){ // Get and update score, station, tickets, line with cookies
+    var score = null;
+    var line = null;
+    
+}
+
+function get_question(){ // 
+    
+}
