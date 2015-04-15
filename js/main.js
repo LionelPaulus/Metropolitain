2ebab56e9d914ac237556bdd3f77f569793f517d
Luca <haenell@yahoo.fr>
2015-04-15 15:56:44 +0200

    fixes

--- js/main.js
+++ js/main.js
@@ -180,7 +180,7 @@ function chrono() {
         duree--;
     }
 
-    $('#temps').html(duree);
+    $('#temps').html(duree +"'");
     $('#statio ').html("station=" + station);
 
     if (station == num_stations) {
